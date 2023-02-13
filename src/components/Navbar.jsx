71dd1dcecea1e51fe9660d2b467f16ee55acc35f
Ma.Fer <myfirstvape.2019@gmail.com>
2023-02-13 18:23:03 -0500

    Creacion del componente Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import "../styles/navbar.css"
+import logonavbar from "../assets/LogoPink3.png"
+
+
+
+const Navbar = () => {
+  const [btnChange,setbtnChange] = useState(false)
+    
+  const[ btnHide, setbtnHide ] = useState(true)
+
+    const btnClick = () =>{
+      setbtnChange(!btnChange) 
+    }
+
+   const body = document.body.className=btnChange ? "dark" : "active"
+
+    const hideClick = () =>{
+      setbtnHide(!btnHide)
+
+    }
+
+  return (
+
+    <nav className={btnChange? "navBarDark":'navbar-container'}>
+      <div className='navLogo' >
+        <img src={logonavbar} alt="" />
+        </div>
+      <ul className='content-navbar' >
+        <li  className='items-navbar' >
+          <a href='#about'>About me</a>
+        </li>
+        <li className='items-navbar'>
+          <a href='#projects'>Projects</a>
+        </li>
+        <li className='items-navbar'>
+          <a href='#my abilities'>My abilities</a>
+        </li>
+        <li className='items-navbar'>
+          <a href='#contact me'>Contact me</a>
+        </li>
+      </ul>
+      <div className='navIcon'>
+          <button onClick={btnClick} className={btnChange ? "switch active" : "switch"} id="switch">
+           <span><i className="fa-solid fa-sun"></i></span>
+           <span><i className="fa-solid fa-moon"></i></span>  
+             </button>
+        
+        </div>
+    </nav>
+  );
+};
+
+export default Navbar;
